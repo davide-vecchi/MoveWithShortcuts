3bd8d66456103b903014bfcd0a7fecd6f70e58ef
Davide Vecchi <132778672+davide-vecchi@users.noreply.github.com>
2026-06-26 00:55:16 +0200

    feat(encoding): Use Windows Terminal instead of batch / PowerShell scripts.
    - The purpose is avoiding Windows Console API encoding bugs in handling Unicode chars. See e.g. JDK-8266674 JDK-8356165 .

---
+++ RenameWithLinks.ps1
@@ -0,0 +1,15 @@
+# PowerShell helper script to launch the executable JAR.
+# This script gets called by a batch file (RenameWithLinks.BAT)
+# which has the following content to call this script :
+#
+# @ECHO OFF
+# wt pwsh -NoExit -File "C:\RenameWithLinks\RenameWithLinks.ps1"
+#
+
+$OutputEncoding = [Console]::InputEncoding = [Console]::OutputEncoding = [System.Text.UTF8Encoding]::new($false)
+
+cd C:\RenameWithLinks\
+
+java -jar C:\RenameWithLinks\RenameWithLinks.jar
+
+# Read-Host "Press Enter to exit"
