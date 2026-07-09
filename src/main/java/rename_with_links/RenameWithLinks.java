42309cc5a032ec807d6bd9603e814e564652a09f
Davide Vecchi <132778672+davide-vecchi@users.noreply.github.com>
2026-07-09 12:15:08 +0200

    style(userIO): Improve user message.

--- src/main/java/rename_with_links/RenameWithLinks.java
+++ src/main/java/rename_with_links/RenameWithLinks.java
@@ -524,8 +524,8 @@ public class RenameWithLinks {
   private @NotNull File askSearchDirectory() throws UserRequestedTermination {
     
     final String searchPath = this.appContext.userIO.in(
-                                                "Enter the path to scan for " + WIN_SHORTCUT_EXTENSION
-                                                        + " shortcuts to update (if the new name entered in the previous question included a path and that caused the folder tree to change, this path to scan must refer to the new folder tree)"
+                                                "Enter the path of the folder to scan for " + WIN_SHORTCUT_EXTENSION
+                                                        + " shortcuts to update (if the \"new name\" entered in the previous question included a path and that causes the folder tree to change, this path to scan must refer to the new folder tree)"
                                                         + " or press Enter for the default, or type " + calcCancelCharsPrompt(CANCEL_CHARS)
                                          , getCurrentFolder(), CANCEL_CHARS);
     if (searchPath == null) {
