8be5d6f6192a40eed01c679736d2939fb7061933
Davide Vecchi <132778672+davide-vecchi@users.noreply.github.com>
2026-07-28 16:37:51 +0200

    improve(scripts): Improve launch scripts.
    - The PS sets the current folder based on what path the BAT passes to it, and the BAT passes its current folder.
    
    - The 3 files (BAT, PS, JAR) are required to be all in the same folder.

--- MoveWithShortcuts.ps1
+++ MoveWithShortcuts.ps1
@@ -1,15 +1,41 @@
 # PowerShell helper script to launch the executable JAR.
-# This script gets called by a batch file (MoveWithShortcuts.BAT)
-# which has the following content to call this script :
+# This script typically gets called by a batch file (MoveWithShortcuts.BAT)
+#  :
 #
 # @ECHO OFF
-# wt pwsh -NoExit -File "C:\MoveWithShortcuts\MoveWithShortcuts.ps1"
+# wt pwsh -NoExit -File "MoveWithShortcuts.ps1"
 #
+
+param(
+    [string]$WorkDir
+)
+
+# Set the output encoding :
 
 $OutputEncoding = [Console]::InputEncoding = [Console]::OutputEncoding = [System.Text.UTF8Encoding]::new($false)
 
-Set-Location C:\MoveWithShortcuts\
+# Handle the setting of the working folder :
 
-java -jar C:\MoveWithShortcuts\MoveWithShortcuts.jar
+if ($WorkDir) {
+
+    # : The -WorkDir argument was passed.
+
+    Set-Location $WorkDir
+}
+else {
+
+    # : The -WorkDir argument was not passed;
+    #   fallback to the folder where this script is :
+
+    Set-Location $PSScriptRoot
+}
+# Start the executable JAR, which must be in the same folder where this script is :
+
+$jarPath = "$PSScriptRoot\MoveWithShortcuts.jar"
+
+[System.Console]::Out.WriteLine()
+[System.Console]::Out.WriteLine("java -jar ""$jarPath""")
+
+java -jar "$jarPath"
 
 # Read-Host "Press Enter to exit"
